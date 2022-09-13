003f21c1d022b236b8393ac8fe98a5461fafb183
abhilash.dubey <abhilash.dubey@nuance.com>
2022-09-13 22:07:03 +0530

    1. Two Sum | Leetcode | Top Interview Questions

--- DataStructures/leetcode_interview/TwoSum.py
+++ DataStructures/leetcode_interview/TwoSum.py
@@ -49,7 +49,6 @@ class Solution:
         return final_list
 
 # Hints:-
-#
 # 1. A really brute force way would be to search for all possible pairs of numbers but that would be too
 # slow. Again, it's best to try out brute force solutions for just for completeness. It is from these brute force
 # solutions that you can come up with optimizations.
