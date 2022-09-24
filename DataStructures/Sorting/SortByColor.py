dbc6f831be9fa4afcd660d8eaa23a0d603787413
abhilash.dubey <abhilash.dubey@nuance.com>
2022-09-25 02:06:06 +0530

    Day 22 - Intermediate DSA : Sorting | Q1. Sort by Color

---
+++ DataStructures/Sorting/SortByColor.py
@@ -0,0 +1,75 @@
+# Q1. Sort by Color
+#
+# Problem Description:-
+#
+# Given an array with N objects colored red, white, or blue, sort them so that objects of the
+# same color are adjacent, with the colors in the order red, white, and blue.
+#
+# We will use the integers 0, 1, and 2 to represent red, white, and blue, respectively.
+#
+# Note: Using the library sort function is not allowed.
+#
+#
+#
+# Problem Constraints
+# 1 <= N <= 1000000
+# 0 <= A[i] <= 2
+#
+#
+# Input Format
+# First and only argument of input contains an integer array A.
+#
+#
+# Output Format
+# Return an integer array in asked order
+#
+#
+# Example Input
+# Input 1 :
+#     A = [0 1 2 0 1 2]
+# Input 2:
+#
+#     A = [0]
+#
+#
+# Example Output
+# Output 1:
+#     [0 0 1 1 2 2]
+# Output 2:
+#
+#     [0]
+#
+#
+# Example Explanation
+# Explanation 1:
+#     [0 0 1 1 2 2] is the required order.
+class Solution:
+    # @param A : list of integers
+    # @return a list of integers
+    def sortColors(self, A):
+        start = 0  # values pointing to Zero
+        mid = 0  # values pointing to One
+        end = len(A) - 1  # values pointing to Two
+
+        while mid <= end:
+            if A[mid] == 2:
+                A[end], A[mid] = A[mid], A[end]
+                end -= 1
+            elif A[mid] == 0:
+                A[mid], A[start] = A[start], A[mid]
+                start += 1
+                mid += 1
+            else:
+                mid += 1
+        return A
+
+# Observation/ Approach Followed:-
+# There are multiple approaches possible here. We need to make sure we do not allocate extra memory.
+#
+# Approach 1:-
+# Count the number of red, white, and blue balls. Then, in another pass, set the initial redCount number of cells as
+# 0, next whiteCount cell as 1, and next blueCount cells as 2. Requires two passes of the array.
+#
+# Approach 2:-
+# Swap the 0s to the start of the array maintaining a pointer, and 2s to the end of the array.
+# 1s will automatically be in their right position.
