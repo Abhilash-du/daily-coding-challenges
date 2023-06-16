c5cd6295cd0ae2d142e6ceb32a5ad16009c388c6
abdubey <abhilash.dubey@nuance.com>
2023-06-16 12:48:11 +0530

    1161. Maximum Level Sum of a Binary Tree | Leetcode

--- DataStructures/Trees/Leetcode/MaxLevelSum.py
+++ DataStructures/Trees/Leetcode/MaxLevelSum.py
@@ -1,7 +1,7 @@
 """
 # Author: Abhilash Dubey
 # GitHub:  https://github.com/Abhilash-du/
-# Description: Python solution to determine the max sum of nodes at level x
+# Description: Python solution to determine the Maximum Level Sum of a Binary Tree
 
 Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
 Return the smallest level x such that the sum of all the values of nodes at level x is maximal.
